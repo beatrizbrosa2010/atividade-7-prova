dcfe4348ea706832895a3b34504fdf37c6b928ef
beatrizbrosa2010 <beatrizbrosa8@gmail.com>
2025-10-17 16:47:43 -0300

    Add files via upload

---
+++ ATIVIDADE 8/ATIVIDADE 8/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+    class Program
+{
+        string static void Main()
+    {
+        string[] nomes = { "Ana", "Bruno", "Carla", "Diego", "Fernanda" };
+
+        Console.Write("Digite um nome para pesquisar: ");
+        string nomeBusca = Console.ReadLine();
+
+        bool encontrado = false;
+
+        foreach (string nome in nomes)
+        {
+            if (nome.Equals(nomeBusca, StringComparison.OrdinalIgnoreCase)) ;
+            {  encontrado = true;
+                break; 
+            }
+        }
+        if (encontrado)
+        {
+            Console.WriteLine("$ O nome {nomeBusca} foi encontrado no vetor: ");
+             }
+        else
+        {
+            Console.WriteLine("$ O nome {nomeBusca} nao esta no vetor.");
+        }
+        }
+}
---
+++ ATIVIDADE 8/ATIVIDADE 8/ProgramHelpers.cs
@@ -0,0 +1,29 @@
+﻿internal static class ProgramHelpers
+{
+    private static void Main(Console console)
+    {
+        string[] nomes = { "Ana", "Bruno", "Carla", "Diego", "Fernanda" };
+
+        Console.Write("Digite um nome para pesquisar: ");
+        string nomeBusca = Console.ReadLine();
+
+        bool encontrado = false;
+
+        foreach (string nome in nomes)
+        {
+            if (nome.Equals(nomeBusca, StringComparison.OrdinalIgnoreCase)) ;
+            {
+                encontrado = true;
+                break;
+            }
+        }
+        if (encontrado)
+        {
+            Console.WriteLine("$ O nome {nomeBusca} foi encontrado no vetor: ");
+        }
+        else
+        {
+            Console.WriteLine("$ O nome {nomeBusca} nao esta no vetor.");
+        }
+    }
+}
