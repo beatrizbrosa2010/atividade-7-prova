e7cebe9546386aafc602025dd48806b8b1570a2e
beatrizbrosa2010 <beatrizbrosa8@gmail.com>
2025-10-17 15:51:38 -0300

    Add files via upload

---
+++ atividade 7/atividade 7/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.Write("Número inicial: ");
+        int inicio = int.Parse(Console.ReadLine());
+
+        Console.Write("Número final: ");
+        int fim = int.Parse(Console.ReadLine());
+
+        for (int i = inicio; i <= fim; i++)
+        {
+            Console.WriteLine($"\nTabuada do {i}:");
+            for (int j = 1; j <= 10; j++)
+                Console.WriteLine($"{i} x {j} = {i * j}");
+        }
+    }
+}
